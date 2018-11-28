0384992d11d9dca60355cb7290f54325ed16059c
Sergi Hernanz <sergi.hernanz@inqbarna.com>
2018-11-28 08:16:29 +0100

    XCode new sample project

---
+++ RoutingNotes/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  RoutingNotes
+//
+//  Created by Sergi Hernanz on 28/11/2018.
+//  Copyright © 2018 Sergi Hernanz. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
